5a5e9de47e15cb520781721f3d3e8c8f990c7f33
Loey <102442925+loeycism@users.noreply.github.com>
2022-11-14 20:21:29 +0000

    v1

---
+++ password_generator.py
@@ -0,0 +1,8 @@
+import random
+
+# caracteres permitidos
+ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+digits = '0123456789'
+punctuation = '#$%&*?@_-'
+
